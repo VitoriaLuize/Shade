ed7c692ab7c92a7d11e78c20a146ff8182e67b7c
VitoriaLuize <vitoria_luize@hotmail.com>
2016-05-23 23:30:10 -0300

    nowisbetter
    a metade dos print

--- README.py
+++ README.py
@@ -1,33 +1,41 @@
 # Shade
-O ŠHɅDΣ é uma ferramenta de auxilio ao grande e pequeno comerciante.Que facilita o entendimento de todo o seu estoque,preços e lucros obtidos  Deixando claro tudo o que você precisa para ter o controle do seu comercio.ᄿ
-
-
-
-print("               ⎝ Olá, Bem vindo ao ŠHɅDΣ⎞                ")
-print(str.upper(input("                  「 JɅ ESTɅ CɅDɅSTRɅDO」: ") ))
-print(str.upper(input("ℒogin : ")))
+print("                  ⎝ ▲ Olá, Bem vindo ao ŠHɅDΣ ▲ ⎞                  ")
 print("ᄽ O ŠHɅDΣ é uma ferramenta de auxilio ao grande e pequeno comerciante.\n Que facilita o entendimento de todo o seu estoque,preços e lucros obtidos \n Deixando claro tudo o que você precisa para ter o controle do seu comercio.ᄿ")
+print(str.upper(input("                  「 JɅ ESTɅ CɅDɅSTRɅDO 」: ") ))
+#if sim
+print(str.upper(input("ℒogin : ")))
+#else
+print("               │ VAMOS SE CɅDɅSTRɅR ? ┃")
+print(str.upper(input("DIGITE O LOGIN DESEJADO: ")))
+#print(variavel)
+print(" PRONTO.Você já foi cadastrado. Volte ao menu principal")
+#o login volta, para poder entrar no menu
+print(str.upper(input("ℒogin : ")))
+#if login in lista,print o menu
+    
+print("_____________________________________________________________")
+print("╳                     ✳   ŠHɅDΣ    ✳                       ╳")
+print("╳            ◄ ⎝ Ⅰ ⎞ CɅDɅSTRɅR PRθDƲTθ                       ╳")
+print("╳            ◄  ⎝ Ⅱ ⎞ PESɊƲISAR PRθDƲTθ                     ╳")
+print("╳            ◄   ⎝ Ⅲ ⎞ PESɊƲISAR PθR PREÇθ                   ╳")           
+print("╳            ◄    ⎝ Ⅳ ⎞ ESTOQUE TOTAL                       ╳")      
+print("╳            ◄     ⎝ Ⅴ ⎞ RETIRADA DE PRθDƲTθS                ╳")          
+print("╳            ◄      ⎝ Ⅵ ⎞ ESTOQUE ATUALIZADO                ╳")
+print("╳            ◄       ⎝ Ⅶ ⎞ CAIXA                             ╳")
+print("╳            ◄        ⎝ Ⅷ ⎞ SAIR                            ╳")
+print("_____________________________________________________________")
 
-print("___________________________________________________________")
-print("╳                     ŠHɅDΣ                              ╳")
-print("╳             ⎝ ⎞CɅDɅSTRɅR PRθDƲTθ                      ╳")
-print("╳              ⎝ ⎞PESɊƲISAR PRθDƲTθ                     ╳")
-""""print("╳           ⎝ ⎞
-print("╳
-print("╳
-print("╳
-print("╳
-print("╳
-print("╳"""
 
+#opção ==1
+print("         │   ✳   ŠHɅDΣ    ✳  ┃")                 
+print("            CɅDɅSTRɅR PRθDƲTθ   ")
+print(str.upper(input("QUANTOS PRODUTOS VÃO SER CADASTRADOS ? ")))
+print(str.upper(input("NθMΣ: ")))
+print(str.upper(input("QUANTIDADΣ: ")))
+print(int(input("VALOR DO PRODUTO:R$ ")))
 
-
-
-
-
-
-
-
-
-#print("         ⎝ Hello, Welcome to the ŠHɅDΣ program ⎞") 
+#opçao==2
+print("         │   ✳   ŠHɅDΣ    ✳  ┃")  
+print("            PESɊƲISAR PRθDƲTθ")
+print(" QUAL PRODUTO VOCÊ PROCURA? ")
 
