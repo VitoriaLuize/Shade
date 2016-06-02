167cf2a903405ab9e9ffd251bd2e5a15698e4abf
viniciusOZ <vini5.pb@hotmail.com>
2016-06-02 19:57:58 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,82 +1,56 @@
 #essa primeira parte esta pronta, a parte do login. please não toca. bjão de luz.
-print("                  ⎝ ▲ Olá, Bem vindo ao ŠHɅDΣ ▲ ⎞                  ")
-print("ᄽ O ŠHɅDΣ é uma ferramenta de auxilio ao grande e pequeno comerciante.\n Que facilita o entendimento de todo o seu estoque,preços e suas atualizações \n Deixando claro tudo o que você precisa para ter o controle do seu comercio.ᄿ")
-cadastro=str.upper(input("                  「 JɅ ESTɅ CɅDɅSTRɅDO 」: ") )
-if cadastro == "SIM":
-    login=str.upper(input("ℒogin : "))
-else:
-    print("               │ VAMOS SE CɅDɅSTRɅR ? ┃")
-    novo_cadastro=str.upper(input("DIGITE O LOGIN DESEJADO: "))
-    print(" PRONTO.",novo_cadastro,"já foi cadastrado. Volte ao menu principal")
-    login=str.upper(input("ℒogin : "))
-if login!= "yeah":
+import arquivar
+
+
+
+def menu():
     print("_____________________________________________________________")
     print("╳                     ✳   ŠHɅDΣ    ✳                       ╳")
-    print("╳            ◄ ⎝ Ⅰ ⎞ CɅDɅSTRɅR PRθDƲTθ                       ╳")
-    print("╳            ◄  ⎝ Ⅱ ⎞ PESɊƲISAR PRθDƲTθ                     ╳")
-    print("╳            ◄   ⎝ Ⅲ ⎞ PESɊƲISAR PθR PREÇθ                   ╳")           
+    print("╳            ◄ ⎝ Ⅰ ⎞ CɅDɅSTRɅR PRθDƲTθ                      ╳")
+    print("╳            ◄  ⎝ Ⅱ ⎞ RETIRADA DE PRθDƲTθS                  ╳")
+    print("╳            ◄   ⎝ Ⅲ ⎞ PESɊƲISAR PθR PREÇθ                  ╳")           
     print("╳            ◄    ⎝ Ⅳ ⎞ ESTOQUE TOTAL                       ╳")      
-    print("╳            ◄     ⎝ Ⅴ ⎞ RETIRADA DE PRθDƲTθS                ╳")          
-    print("╳            ◄      ⎝ Ⅵ ⎞ ESTOQUE ATUALIZADO                ╳")
-    print("╳            ◄        ⎝ VII⎞ SAIR                            ╳")
+    print("╳            ◄     ⎝ Ⅴ ⎞  PESɊƲISAR PRθDƲTθ                 ╳")          
+    print("╳            ◄      ⎝ Ⅵ ⎞ SAIR                              ╳")
     print("_____________________________________________________________")
 
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
-print("                                                                             ")
+def casdastrarProduto():
+    produto=[]
+    print("                 │   ✳   ŠHɅDΣ    ✳   │")                 
+    print("                 │  CɅDɅSTRɅR PRθDƲTθ  │")
+    nome_produto=str.upper(input( "│ NθMΣ: "))
+    quantidade=int(input( "│ QUANTIDADΣ: "))
+    preco=int(input(       "│ VALOR DO PRODUTO:R$ "))
+    codigo=int(input("       │ CθDIGθ: "))
+    produto=nome_produto,quantidade,preco,codigo
+    return produto
 
-# acaba aqui no momento
-
-'''#opção ==1  
-print("                 │   ✳   ŠHɅDΣ    ✳   │")                 
-print("                 │  CɅDɅSTRɅR PRθDƲTθ  │")
-print(str.upper(input(" │ QUANTOS PRODUTOS VÃO SER CADASTRADOS ? ")))
-#for i in range
-print(str.upper(input( "│ NθMΣ: ")))
-print(str.upper(input( "│ QUANTIDADΣ: ")))
-print(int(input(       "│ VALOR DO PRODUTO:R$ ")))
-
-#opçao==2
-print("                         │   ✳   ŠHɅDΣ    ✳    │")  
-print("                         │   PESɊƲISAR PRθDƲTθ  │")
-print(str.upper(input("         │  QUAL PRODUTO VOCÊ PROCURA? ")))
-
-#opçao==3
-print("          │   ✳   ŠHɅDΣ    ✳       │ ")
-print("          │    PESɊƲISAR PθR PREÇθ  │ ")
-print(int(input("│  QUAL O PREÇO QUE VOCÊ QUER? R$")))
-
-#opçao==4
-print("                    │   ✳   ŠHɅDΣ    ✳       │ ")
-print("                    │    ESTOQUE TOTAL        │ ")
-print(str.upper(input("    │ DESEJA EXIBIR TODO O ESTOQUE? ")))
-print("│ESTOQUE TOTAL :")
+def apagarProduto(nome,num,produtos):
+    lista = []
+    lista = arquivar.apagarNome(nome,num,produtos)
+    return lista
     
-
-#opção==5
-
-print("                 │   ✳   ŠHɅDΣ    ✳       │ ")
-print("                 │    RETIRADA DE PRθDƲTθS │ ")
-print(int(input("       │QUANTOS PRODUTOS VÃO SER RETIRADOS? ")))
-print(str.upper(input(" │ TEM CERTEZA QUE VAI QUERER RETIRAR ELES? ")))
-#if
-print("                 │ PRODUTOS RETIRADOS │")
-
-
-#opção==6
-print("    │   ✳   ŠHɅDΣ    ✳       │ ")
-print("    │ ESTOQUE ATUALIZADO      │ ")
-print(str.upper(input(" │ DESEJA EXIBIR O ATUAL ESTOQUE? ")))
-print("│NOVO ESTOQUE: ")
-
-#opção==7
-print(" │ FOI UM PRAZER PARA NÓS, TER VOCÊ COMO USUARIO. ATÉ A PROXIMA VISITA. │")
-
-
-
-
+    
+      
+menu()
+produtos = []
+produtos = arquivar.ler()
+produto = []
+continuar=True
+while continuar:
+    opcao=str.upper(input("  │ DIGITE SUA OPÇÃO:  │"))
+    if (opcao=="1"):
+        arquivar.armazenar(casdastrarProduto())
+    elif (opcao=="2"):
+        print(" │ DESEJA RETIRAR POR NOME OU CODIGO? ")
+        print("     │ DIGITE 1 PARA NOME. │              ")
+        print("     │ DIGITE 2 PARA CODIGO│              ")
+        ask = input()
+        if (ask=="1"):
+            produtos = arquivar.ler()
+            nome=str.upper(input("│ DIGITE O NOME DO PRODUTO: "))
+            arquivar.atualizarProdutos(apagarProduto(nome,0,produtos))              
+        elif (ask=="2"):
+            produtos = arquivar.ler()
+            codigo=int(input("│ DIGITE O NOME DO PRODUTO: "))
+            arquivar.atualizarProdutos(apagarProduto(codigo,3,produtos))  
