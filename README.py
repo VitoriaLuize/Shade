8829a0466a999c66308d147481228052099e4dff
viniciusOZ <vini5.pb@hotmail.com>
2016-06-07 17:57:42 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,56 +1,75 @@
-#essa primeira parte esta pronta, a parte do login. please não toca. bjão de luz.
-import arquivar
-
-
-
-def menu():
-    print("_____________________________________________________________")
-    print("╳                     ✳   ŠHɅDΣ    ✳                       ╳")
-    print("╳            ◄ ⎝ Ⅰ ⎞ CɅDɅSTRɅR PRθDƲTθ                      ╳")
-    print("╳            ◄  ⎝ Ⅱ ⎞ RETIRADA DE PRθDƲTθS                  ╳")
-    print("╳            ◄   ⎝ Ⅲ ⎞ PESɊƲISAR PθR PREÇθ                  ╳")           
-    print("╳            ◄    ⎝ Ⅳ ⎞ ESTOQUE TOTAL                       ╳")      
-    print("╳            ◄     ⎝ Ⅴ ⎞  PESɊƲISAR PRθDƲTθ                 ╳")          
-    print("╳            ◄      ⎝ Ⅵ ⎞ SAIR                              ╳")
-    print("_____________________________________________________________")
-
 def casdastrarProduto():
-    produto=[]
-    print("                 │   ✳   ŠHɅDΣ    ✳   │")                 
-    print("                 │  CɅDɅSTRɅR PRθDƲTθ  │")
-    nome_produto=str.upper(input( "│ NθMΣ: "))
-    quantidade=int(input( "│ QUANTIDADΣ: "))
-    preco=int(input(       "│ VALOR DO PRODUTO:R$ "))
-    codigo=int(input("       │ CθDIGθ: "))
-    produto=nome_produto,quantidade,preco,codigo
-    return produto
+    arquivo = open("produtos.txt","a")
+    print("◄ CɅDɅSTRɅR PRθDƲTθ")
+    codigo=input("Digite um CθDIGθ: ")
+    while(codigo+"\n" in criaListaArquivo()):
+        print("Esse código já está cadastrado !")
+        codigo=input("Digite umm novo CθDIGθ: ")
+    nome_produto=str.upper(input( "NθMΣ: "))
+    quantidade=int(input( "QUANTIDADΣ: "))
+    preco=float(input(       "VALOR DO PRODUTO:R$ "))
+    arquivo.writelines([codigo+"\n", nome_produto+"\n",str(quantidade)+"\n",str(preco)+"\n"])
+    arquivo.close()
 
-def apagarProduto(nome,num,produtos):
-    lista = []
-    lista = arquivar.apagarNome(nome,num,produtos)
-    return lista
+def criaListaArquivo():
+    arquivo = open("produtos.txt","r")
+    produtos = arquivo.readlines()
+    arquivo.close()
+    return produtos
+        
+def consultarProduto():
+    codigo=input("PESQUISE O PRODUTO POR CÓDIGO: ")
+    produtos = criaListaArquivo()
+    for i in range(0,len(produtos),4):
+        if(produtos[i] == codigo+"\n"):
+            print("Código: ", produtos[i][:len(produtos[i])-1])
+            print("Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
+            print("Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
+            print("Preço: ", produtos[i+3][:len(produtos[i+3])-1])
+            break
+
+def listarEstoque():
+    produtos = criaListaArquivo()
+    for i in range(0,len(produtos),4):
+        print("Código: ", produtos[i][:len(produtos[i])-1])
+        print("Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
+        print("Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
+        print("Preço: ", produtos[i+3][:len(produtos[i+3])-1]+"\n")
+
+def alterarProdutoPorCodigo():
+    produtos = criaListaArquivo()
+
+    print("◄ ALTERAR PRθDƲTθ")
+    codigo = input("DIGITE O CODIGO DO PRODUTO QUE DESEJA ALTERAR: ")
+
+    for i in range(0,len(produtos),4):
+        if (int(produtos[i]) == int(codigo)):
+            novocodigo = input("NOVO CODIGO:")+"\n"
+            produtos[i] = novocodigo
+            produtos[i+1] = str.upper(input( "NOVO NθMΣ: "))+"\n"
+            produtos[i+2] = input( "NOVA QUANTIDADΣ: ")+"\n"
+            produtos[i+3] = input("NOVO VALOR DO PRODUTO:R$ ")+"\n"
+            arquivo = open("produtos.txt","w")
+            arquivo.writelines(produtos)
+            arquivo.close()
+            break
+     
+def cls():
+    for i in range(50):
+        print()
+        
+def apagarProduto():
+    produtos = criaListaArquivo()
+    print("◄ APAGAR PRθDƲTθ")
+    codigo = input("DIGITE O CODIGO DO PRODUTO QUE DESEJA APAGAR: ")
     
-    
-      
-menu()
-produtos = []
-produtos = arquivar.ler()
-produto = []
-continuar=True
-while continuar:
-    opcao=str.upper(input("  │ DIGITE SUA OPÇÃO:  │"))
-    if (opcao=="1"):
-        arquivar.armazenar(casdastrarProduto())
-    elif (opcao=="2"):
-        print(" │ DESEJA RETIRAR POR NOME OU CODIGO? ")
-        print("     │ DIGITE 1 PARA NOME. │              ")
-        print("     │ DIGITE 2 PARA CODIGO│              ")
-        ask = input()
-        if (ask=="1"):
-            produtos = arquivar.ler()
-            nome=str.upper(input("│ DIGITE O NOME DO PRODUTO: "))
-            arquivar.atualizarProdutos(apagarProduto(nome,0,produtos))              
-        elif (ask=="2"):
-            produtos = arquivar.ler()
-            codigo=int(input("│ DIGITE O NOME DO PRODUTO: "))
-            arquivar.atualizarProdutos(apagarProduto(codigo,3,produtos))  
+    for i in range(0,len(produtos),4):
+        if (int(produtos[i]) == int(codigo)):
+            del produtos[i]
+            del produtos[i]
+            del produtos[i]
+            del produtos[i]
+            arquivo = open("produtos.txt","w")
+            arquivo.writelines(produtos)
+            arquivo.close()
+            break
