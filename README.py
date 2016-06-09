1c43874c278dcec95069bcd2ff9c4d30dfe2f231
VitoriaLuize <vitoria_luize@hotmail.com>
2016-06-09 20:00:42 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,15 @@
 def casdastrarProduto():
     arquivo = open("produtos.txt","a")
-    print("◄ CɅDɅSTRɅR PRθDƲTθ")
-    codigo=input("Digite um CθDIGθ: ")
+    print("                   ▉CɅDɅSTRɅR PRθDƲTθ▉")
+    codigo=input(" ▉ Digite um CθDIGθ(ex.00000): ")
     while(codigo+"\n" in criaListaArquivo()):
-        print("Esse código já está cadastrado !")
-        codigo=input("Digite umm novo CθDIGθ: ")
-    nome_produto=str.upper(input( "NθMΣ: "))
-    quantidade=int(input( "QUANTIDADΣ: "))
-    preco=float(input(       "VALOR DO PRODUTO:R$ "))
+        print(" ▉Esse código já está cadastrado ! ▉")
+        codigo=input(" ▉Digite um novo CθDIGθ: ")
+
+    nome_produto=str.upper(input( " ▉NθMΣ: "))
+    quantidade=int(input( " ▉QUANTIDADΣ: "))
+    preco=float(input(       " ▉VALOR DO PRODUTO:R$ "))
+    print("                   ▉PRODUTO CADASTRADO COM SUCESSO ▉")
     arquivo.writelines([codigo+"\n", nome_produto+"\n",str(quantidade)+"\n",str(preco)+"\n"])
     arquivo.close()
 
@@ -18,41 +20,52 @@ def criaListaArquivo():
     return produtos
         
 def consultarProduto():
-    codigo=input("PESQUISE O PRODUTO POR CÓDIGO: ")
+    achou = False
+    codigo=input(" ▉PESQUISE O PRODUTO POR CÓDIGO ▉: ")
     produtos = criaListaArquivo()
     for i in range(0,len(produtos),4):
         if(produtos[i] == codigo+"\n"):
-            print("Código: ", produtos[i][:len(produtos[i])-1])
-            print("Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
-            print("Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
-            print("Preço: ", produtos[i+3][:len(produtos[i+3])-1])
+            print(" ▉Código: ", produtos[i][:len(produtos[i])-1])
+            print(" ▉Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
+            print(" ▉Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
+            print(" ▉Preço:R$ ", produtos[i+3][:len(produtos[i+3])-1])
+            achou = True
             break
+    if(achou == False):
+        print("               ▉PRODUTO NÃO ENCONTRADO ▉ ")
+        
 
 def listarEstoque():
     produtos = criaListaArquivo()
     for i in range(0,len(produtos),4):
-        print("Código: ", produtos[i][:len(produtos[i])-1])
-        print("Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
-        print("Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
-        print("Preço: ", produtos[i+3][:len(produtos[i+3])-1]+"\n")
+        print(" ▉Código: ", produtos[i][:len(produtos[i])-1])
+        print(" ▉Descrição: ", produtos[i+1][:len(produtos[i+1])-1])
+        print(" ▉Quantidade: ", produtos[i+2][:len(produtos[i+2])-1])
+        print(" ▉Preço:R$ ", produtos[i+3][:len(produtos[i+3])-1]+"\n")
+    if(len(produtos)==0):
+        print("               ▉NENHUM PRODUTO CADASTRADO.DIGITE (1) PARA CADASTRAR ▉")
 
 def alterarProdutoPorCodigo():
     produtos = criaListaArquivo()
+    achou= False
 
-    print("◄ ALTERAR PRθDƲTθ")
-    codigo = input("DIGITE O CODIGO DO PRODUTO QUE DESEJA ALTERAR: ")
+    print(" ▉ALTERAR PRθDƲTθ ▉")
+    codigo = input(" ▉DIGITE O CODIGO DO PRODUTO QUE DESEJA ALTERAR ▉: ")
 
     for i in range(0,len(produtos),4):
-        if (int(produtos[i]) == int(codigo)):
-            novocodigo = input("NOVO CODIGO:")+"\n"
+        if (int(produtos[i][:len(produtos[i])-1]) == int(codigo)):
+            novocodigo = input(" ▉NOVO CODIGO:")+"\n"
             produtos[i] = novocodigo
-            produtos[i+1] = str.upper(input( "NOVO NθMΣ: "))+"\n"
-            produtos[i+2] = input( "NOVA QUANTIDADΣ: ")+"\n"
-            produtos[i+3] = input("NOVO VALOR DO PRODUTO:R$ ")+"\n"
+            produtos[i+1] = str.upper(input( " ▉NOVO NθMΣ: "))+"\n"
+            produtos[i+2] = input( " ▉NOVA QUANTIDADΣ: ")+"\n"
+            produtos[i+3] = input(" ▉NOVO VALOR DO PRODUTO:R$ ")+"\n"
             arquivo = open("produtos.txt","w")
             arquivo.writelines(produtos)
             arquivo.close()
+            acho=True
             break
+    if(achou== False):
+        print("                     ▉PRODUTO ALTERADO ▉")
      
 def cls():
     for i in range(50):
@@ -60,8 +73,9 @@ def cls():
         
 def apagarProduto():
     produtos = criaListaArquivo()
-    print("◄ APAGAR PRθDƲTθ")
-    codigo = input("DIGITE O CODIGO DO PRODUTO QUE DESEJA APAGAR: ")
+    achou=False
+    print(" ▉APAGAR PRθDƲTθ ▉")
+    codigo = input(" ▉DIGITE O CODIGO DO PRODUTO QUE DESEJA APAGAR ▉: ")
     
     for i in range(0,len(produtos),4):
         if (int(produtos[i]) == int(codigo)):
@@ -69,7 +83,19 @@ def apagarProduto():
             del produtos[i]
             del produtos[i]
             del produtos[i]
+            print("           ▉PRODUTO APAGADO COM SUCESSO ▉")
             arquivo = open("produtos.txt","w")
             arquivo.writelines(produtos)
             arquivo.close()
+            achou=True
             break
+    if(achou==False):
+        print("               ▉PRODUTO NÃO ENCONTRADO ▉")
+def sobreNos():
+    print("                      │ Olá, Seja Bem-vindo ao  ŠHɅDΣ   ")
+    print("                      │          ▉SOBRE NÓS ▉                            ")
+    print("│O ŠHɅDΣ é uma ferramenta de auxilio ao grande e pequeno comerciante.          │")
+    print("│Que facilita o entendimento de todo o seu estoque,preços e suas atualizações  │")
+    print("│Deixando claro tudo o que você precisa para ter o controle do seu comercio.   │")
+
+
